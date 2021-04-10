4280f75bc9b1fa1d545a7975477fb547427b42a1
Konstantin <richardocouls@gmail.com>
2021-04-10 17:01:50 +0300

    -

--- src/main/kotlin/DBHelper.kt
+++ src/main/kotlin/DBHelper.kt
@@ -34,6 +34,47 @@ class DBHelper(
 
     }
 
+    fun printScholarship(){
+        println("Стипендии по результатам последней сессии")
+        val s = connection?.createStatement()
+        val rs = s?.executeQuery(
+            "SELECT `id`,`last_name`,`first_name`,`mid_name`,`group_id`,`scholarship` FROM students JOIN (SELECT `student_id`, \n" +
+                    " (CASE \n" +
+                    "\t WHEN MIN(`result`) = 5 THEN 3100 \n" +
+                    "\t WHEN MIN(`result`) = 4 THEN 2100\n" +
+                    " \tELSE 0\n" +
+                    " END) AS `scholarship`\n" +
+                    "\n" +
+                    "FROM (SELECT `student_id`, `score`, `attempt`, `reporting_form`,\n" +
+                    " (CASE \n" +
+                    "  \tWHEN `reporting_form` = \"экзамен\" AND `score` > 86 AND `attempt` = 1 THEN 5 \n" +
+                    "  \tWHEN `reporting_form` = \"экзамен\" AND `score` > 70 AND `attempt` = 1 THEN 4\n" +
+                    "  \tWHEN `reporting_form` = \"экзамен\" AND `score` > 55 AND `attempt` = 1 THEN 3\n" +
+                    "  \tWHEN `reporting_form` = \"зачет\" AND `score` > 55 AND `attempt` = 1 THEN 5\n" +
+                    "  \tELSE 0\n" +
+                    "  END) AS `result` \n" +
+                    " FROM performance JOIN disciplines_plans JOIN \n" +
+                    " \t(SELECT `students`.`id`, current_semester FROM `students` JOIN\n" +
+                    "    \t(SELECT year, groups.id, 2*(YEAR(NOW()) - year) - (CASE WHEN MONTH(NOW()) > 6 OR MONTH(NOW()) = 1 THEN 1 ELSE 0 END) AS `current_semester` \n" +
+                    "\t\t\tFROM `groups` JOIN `academic_plans` ON `groups`.`academic_plan_id` = `academic_plans`.`id`) AS `group_cs` \n" +
+                    "                            ON `students`.`group_id` = `group_cs`.`id`) AS `swcs` ON performance.student_id = swcs.id \n" +
+                    "                            AND disciplines_plans.id = performance.disciplines_plan_id \n" +
+                    "                            AND swcs.current_semester = disciplines_plans.semester_number + 1) AS `rank` GROUP BY `student_id`) AS `swsh` ON `students`.`id` = swsh.student_id ORDER BY group_id, last_name,first_name, mid_name")
+
+
+        while (rs?.next() == true){
+            println("ID: ${rs.getInt("id")}\t" +
+                    "${rs.getString("last_name")}\t" +
+                    "${rs.getString("first_name")}\t" +
+                    "${rs.getString("mid_name")}\t" +
+                    "${rs.getString("group_id")}\t" +
+                    "${rs.getInt("scholarship")}\t"
+            )
+        }
+
+
+    }
+
     /**Функция, создающая таблицы в базе данных на основе SQL-дампа
      * @param path путь до SQL-дампа*/
     fun createDataBaseFromDump(path: String){
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,18 +1,21 @@
 //MySQL Server 127.0.0.1:3306
+
+
 fun main() {
     //val db = DBController("test")
     val dbHelper = DBHelper("students")
     dbHelper.apply {
-        dropAllTables()
-        createDataBaseFromDump("src/main/kotlin/students.sql")
-        fillTableFromCSV("cathedras", "data/cathedras.csv")
-        fillTableFromCSV("disciplines", "data/disciplines.csv")
-        fillTableFromCSV("specializations", "data/specializations.csv")
-        fillTableFromCSV("academic_plans", "data/academic_plans.csv")
-        fillTableFromCSV("groups", "data/groups.csv")
-        fillTableFromCSV("disciplines_plans", "data/disciplines_plans.csv")
-        fillTableFromCSV("students", "data/students.csv")
-        fillTableFromCSV("performance", "data/performance.csv")
+//        dropAllTables()
+//        createDataBaseFromDump("src/main/kotlin/students.sql")
+//        fillTableFromCSV("cathedras", "data/cathedras.csv")
+//        fillTableFromCSV("disciplines", "data/disciplines.csv")
+//        fillTableFromCSV("specializations", "data/specializations.csv")
+//        fillTableFromCSV("academic_plans", "data/academic_plans.csv")
+//        fillTableFromCSV("groups", "data/groups.csv")
+//        fillTableFromCSV("disciplines_plans", "data/disciplines_plans.csv")
+//        fillTableFromCSV("students", "data/students.csv")
+//        fillTableFromCSV("performance", "data/performance.csv")
+        printScholarship()
     }
 
 }
